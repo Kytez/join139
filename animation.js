834b27c788ebebbe8881253a3ac85530ffed0449
Marcel Luwinski <marcel.luwinski@gmail.com>
2024-04-04 10:30:51 +0200

    updated style.css

--- animation.js
+++ animation.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", () => {
-    document.getElementById('loadAnimation').classList.add("loader--hidden");
-    });
-    
+  document.getElementById("loadAnimation").classList.add("loader-hidden");
+  setTimeout(() => {
+    document.getElementById("loadAnimation").classList.add("d-none");
+  }, 1000);
+});
