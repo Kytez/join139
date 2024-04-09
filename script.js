ca08c35d5565286e35be2238fb933e93c893f4dd
Marcel Luwinski <marcel.luwinski@gmail.com>
2024-04-09 16:24:48 +0200

    added remote storage

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const STORAGE_TOKEN = 'BWWMEZDTZCQQOXJAJAFR8E89G4VCXBKCXC2VP92F';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, value) {
+    const payload = {key, value, token: STORAGE_TOKEN};
+    return fetch(STORAGE_URL, {method: 'POST', body: JSON.stringify(payload)})
+    .then(response => response.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(response => response.json());
+}
+
+
+
+
+
+
+
+
+
+
 async function includeHTML() {
   let includeElements = document.querySelectorAll('[w3-include-html]');
   for (let i = 0; i < includeElements.length; i++) {
