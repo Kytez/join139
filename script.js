1ebaa4a04ad030247c866ceeaa8c5a4ab3a4a4bb
Marcel Luwinski <marcel.luwinski@gmail.com>
2024-04-10 14:19:39 +0200

    added backend and sign up function

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const STORAGE_TOKEN = 'BWWMEZDTZCQQOXJAJAFR8E89G4VCXBKCXC2VP92F';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 
+let users = [];
+
+
+async function init() {
+    loadUsers();
+}
+
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e) {
+        console.error('Loading error:', e);
+    }
+}
+
 
 async function setItem(key, value) {
     const payload = {key, value, token: STORAGE_TOKEN};
@@ -10,16 +25,50 @@ async function setItem(key, value) {
 
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(response => response.json());
+    return fetch(url).then(response => response.json()).then(response => {
+        if (response.data) { 
+            return response.data.value;
+        } throw `Could not find data with key "${key}".`;
+    });
 }
 
 
+async function signUp() {
+    let name = document.getElementById('nameInputSignUp');
+    let email = document.getElementById('emailInputSignUp');
+    let password = document.getElementById('passwordInputSignUp');
+    let btnSignUp = document.getElementById('btnSignUp');
 
+    btnSignUp.disabled = true;
 
+    users.push({
+        name: name.value,
+        email: email.value,
+        password: password.value
+    });
 
+    await setItem('users', JSON.stringify(users));
 
+    resetForm(name, email, password, btnSignUp);
+    moveToLogIn();
+}
 
+function resetForm(name, email, password, btnSignUp) {
+    let confirmPassword = document.getElementById('confirmPasswordInputSignUp');
+    let privacyCheckboxInputSignUp = document.getElementById('privacyCheckboxInputSignUp');
 
+    name.value = '';
+    email.value = '';
+    password.value = '';
+    confirmPassword.value = '';
+    privacyCheckboxInputSignUp.checked = false;
+
+    btnSignUp.disabled = false;
+}
+
+function moveToLogIn() {
+    window.location.href = '../index.html';
+}
 
 
 async function includeHTML() {
