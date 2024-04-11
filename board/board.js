be74b6c19911788a2d953438ae2a33228ff98224
NoFutureDan <dnwrs.rec123@gmail.com>
2024-04-11 17:43:22 +0200

    %*

--- board/board.js
+++ board/board.js
@@ -60,6 +60,24 @@ function showTask() {
     }
 }
 
+
+function hideTask() {
+    document.getElementById('tasks').style.display = 'none';
+    document.getElementById('taskPopUp').style.display = 'none';
+    
+    var popUpElements = document.getElementsByClassName('pop-up');
+    for (var i = 0; i < popUpElements.length; i++) {
+        // Setze die Transition-Eigenschaft zuerst
+        popUpElements[i].style.transition = 'transform 400ms';
+        // Verwende eine Funktionsschließung, um den Wert von i zu erfassen
+        (function(index) {
+            setTimeout(function() {
+                popUpElements[index].style.transform = 'translateX(200%)';
+            }, 100);
+        })(i);
+    }
+}
+
 function addTask() {
     document.getElementById('addTaskSection').style.display = 'flex';
     document.getElementById('addTaskPopUp').style.display = 'flex';
@@ -72,6 +90,23 @@ function addTask() {
         (function(index) {
             setTimeout(function() {
                 popUpElements[index].style.transform = 'translateX(0)';
+            }, 100);
+        })(i);
+    }
+}
+
+function hideTaskMenu() {
+    document.getElementById('addTaskSection').style.display = 'none';
+    document.getElementById('addTaskPopUp').style.display = 'none';
+    
+    var popUpElements = document.getElementsByClassName('add-task-card');
+    for (var i = 0; i < popUpElements.length; i++) {
+        // Setze die Transition-Eigenschaft zuerst
+        popUpElements[i].style.transition = 'transform 400ms';
+        // Verwende eine Funktionsschließung, um den Wert von i zu erfassen
+        (function(index) {
+            setTimeout(function() {
+                popUpElements[index].style.transform = 'translateX(275%)';
             }, 100);
         })(i);
     }
