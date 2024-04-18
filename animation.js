dbc135917f436c33ee8b65580301c38357d27e70
Marcel Luwinski <marcel.luwinski@gmail.com>
2024-04-18 15:23:41 +0200

    fixed start animation

--- animation.js
+++ animation.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", () => {
-  document.getElementById("loadAnimation").classList.add("loader-hidden");
+  setTimeout(() => {
+    document.getElementById("loadAnimation").classList.add("loader-hidden");
+  }, 200);
   setTimeout(() => {
     document.getElementById("loadAnimation").classList.add("d-none");
   }, 1000);
